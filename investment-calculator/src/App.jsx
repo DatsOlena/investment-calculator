fd13ee9d1340838aa9e10d760f0ca5bf90364b12
DatsOlena <datsolena@gmail.com>
2026-03-06 18:33:04 +0100

    Output table contitionally

--- investment-calculator/src/App.jsx
+++ investment-calculator/src/App.jsx
@@ -9,20 +9,23 @@ function App() {
     'annualInvestment': 1200,
     'expectedReturn': 6,
     'duration': 10,
-});
+  });
 
-function handleChange(inputId, value) {
-  setUserInput((prevInput) => ({
-    ...prevInput,
-    [inputId]: +value,
-  }));
-}
+  function handleChange(inputId, value) {
+    setUserInput((prevInput) => ({
+      ...prevInput,
+      [inputId]: +value,
+    }));
+  }
+
+  const inputIsValid = userInput.duration >= 0;
 
   return (
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
-      <ResultsTable input={userInput} />
+      {inputIsValid ? <ResultsTable input={userInput} /> : <p className="center">Please enter a valid duration greater than 0.</p>}
+
     </>
   )
 }
